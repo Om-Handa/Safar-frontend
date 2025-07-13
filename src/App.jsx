fe732657f1372ee4691cc9d63757202095d3b027
Om-handa <omhanda246@gmail.com>
2025-07-13 11:17:07 +0530

    test10

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,11 @@ import Footer from './Components/Footer'
 
 function App() {
   return (
-    <div className='flex flex-col min-h-screen bg-[#f2ede6]'>
+    <>
       <Navbar />
-
-      <div className='flex-grow'>
         <Outlet />
-      </div>
-
       <Footer />
-    </div>
+    </>
   )
 }
 
